707b85e8761634d27746e9aa19558536a0d252cb
naitoDoriso <desaferreirav@gmail.com>
2023-08-12 19:36:35 -0300

    responsividade

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ $(function(){
         $("#btn_dados").attr('disabled','true');
         let num = Math.floor(Math.random() * 6 + 1);
         $(":root").css('--dado_left',-100*(num-1)-(num+1)+'%');
+        $(":root").css('--dado_left_mb',-100*(num-1)+(num+1)+'%');
         setTimeout(()=>{
             $(".bloco").css('animation','');
             $("#btn_dados").removeAttr('disabled');
