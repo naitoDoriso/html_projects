6940f83dc71c70dd4e882225db76411d6df3fd18
naitoDoriso <desaferreirav@gmail.com>
2023-08-12 15:49:34 -0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    function exibeAleatorio(){
+        $(".bloco").css('animation','rodar .4s ease-in-out forwards');
+        $("#btn_dados").attr('disabled','true');
+        let num = Math.floor(Math.random() * 6 + 1);
+        $(":root").css('--dado_left',-100*(num-1)-(num+1)+'%');
+        setTimeout(()=>{
+            $(".bloco").css('animation','');
+            $("#btn_dados").removeAttr('disabled');
+        },2000);
+    }
+
+    $("#btn_dados").click(exibeAleatorio);
+    
+})
