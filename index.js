16a766d1b6e0f507f2d4c6edbdcec04dc9acd709
naitoDoriso <desaferreirav@gmail.com>
2023-08-27 12:35:58 -0300

    db oficial

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 $(function(){
-
     function exibeAleatorio(){
         $(".bloco").css('animation','rodar .4s ease-in-out forwards');
         $("#btn_dados").attr('disabled','true');
@@ -11,6 +10,26 @@ $(function(){
             $("#btn_dados").removeAttr('disabled');
         },2000);
     }
+
+    if (!localStorage.getItem("db")) {
+        localStorage.setItem("db","db_teste.json");
+    } else {
+        if (localStorage.getItem("db") != "db.json") {
+            $(".change-db").text("Usar Banco de Dados Oficial");
+        } else {
+            $(".change-db").text("Usar Banco de Dados de Teste");
+        }
+    }
+
+    $(".change-db").click(()=>{
+        if (localStorage.getItem("db") != "db.json") {
+            localStorage.setItem("db","db.json");
+            $(".change-db").text("Usar Banco de Dados de Teste");
+        } else {
+            localStorage.setItem("db","db_teste.json");
+            $(".change-db").text("Usar Banco de Dados Oficial");
+        }
+    });
 
     $("#btn_dados").click(exibeAleatorio);
     
